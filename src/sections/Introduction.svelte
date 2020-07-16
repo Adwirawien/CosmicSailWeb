7d36e9f9a50216a8d1797b029a8956f55f858154
Adrian <hannsadrian@gmail.com>
2020-07-16 17:32:53 +0200

    Add introduction section

---
+++ src/sections/Introduction.svelte
@@ -0,0 +1,15 @@
+<section id="introduction" class="min-h-screen flex justify-center text-gray-300 font-fira">
+  <div class="p-5 py-10 my-auto max-w-md">
+    <h1 class="font-semibold mb-4">⛵️ The Idea.</h1>
+    <p class="text-sm">
+      A while ago I found an old rusty rc sailboat on my attic without any controls or batteries.
+      So I decided to burn my money with making this boat as cool and advanced as possible...<br/><br/>
+
+      At first I went over a few ordinary systems with radio control,
+      but I noticed very quickly that the problem with those radio controls is that they lose signal after about 50 meters.
+      In my case that may was related to the antennas being located somewhat below the waterline, blocking the signal.<br/><br/>
+
+      That was a bit disappointing but nevertheless I brainstormed for a better solution and
+      came up with the idea of controlling the boat and its motors with a Raspberrypi.</p>
+  </div>
+</section>
