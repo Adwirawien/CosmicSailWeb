8f985b9d7ebf0bec89abe8ff0be733fca9b285da
Adrian <hannsadrian@gmail.com>
2020-07-16 17:33:31 +0200

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Tailwindcss from './Tailwindcss.svelte'
+  import Header from "./sections/Header.svelte";
+  import Introduction from "./sections/Introduction.svelte";
+  import Features from "./sections/Features.svelte";
+  import Control from "./sections/features/Control.svelte";
+</script>
+
+<Tailwindcss/>
+<main class="w-full">
+  <Header/>
+  <Introduction/>
+  <Features/>
+  <div>
+    <Control/>
+  </div>
+</main>
