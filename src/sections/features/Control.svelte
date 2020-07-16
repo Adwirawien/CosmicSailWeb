f1cf23b9d3c1e35b451285d1d1d0b90f11ea55a0
Adrian <hannsadrian@gmail.com>
2020-07-16 17:32:16 +0200

    Add remote control feature detail

---
+++ src/sections/features/Control.svelte
@@ -0,0 +1,15 @@
+<section id="control" class="min-h-screen flex justify-center text-gray-300 font-fira">
+  <div class="p-5 py-10 my-auto">
+    <div class="sm:flex">
+      <img style="height:10%;width:225px;" class="mx-auto sm:mx-10 my-5" src="/control_mockup.png" alt="mockup"/>
+      <div class="my-auto max-w-md">
+        <h1 class="font-semibold mb-4">📱 Remote control</h1>
+        <p class="text-sm">
+          With CosmicSail it is easily possible to control your boat from your smartphone over the internet.
+          CosmicSail can furthermore display sensor data, for example the current GPS position,
+          speed and even used bandwidth.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
