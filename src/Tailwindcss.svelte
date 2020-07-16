3a752662fcd1fd5f1a070c5643abfb3526506765
Adrian <hannsadrian@gmail.com>
2020-07-16 17:31:30 +0200

    Configure Tailwindcss

---
+++ src/Tailwindcss.svelte
@@ -0,0 +1,14 @@
+<style global>
+  @import url(https://cdn.jsdelivr.net/gh/tonsky/FiraCode@4/distr/fira_code.css);
+
+  @tailwind base;
+  @tailwind components;
+
+  .font-fira { font-family: 'Fira Code', monospace; }
+
+  @supports (font-variation-settings: normal) {
+    .font-fira { font-family: 'Fira Code VF', monospace; }
+  }
+
+  @tailwind utilities;
+</style>
