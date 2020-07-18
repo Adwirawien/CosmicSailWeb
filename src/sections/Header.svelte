ab4b84bbddb15b361adc5a8717ac583aa99e5d84
Adwirawien <hannsadrian@gmail.com>
2020-07-18 13:52:04 +0200

    Update Header Section

--- src/sections/Header.svelte
+++ src/sections/Header.svelte
@@ -12,11 +12,11 @@
     right: 0px;
     bottom: 0px;
     left: 0px;
-    animation: background-fade 10s ease;
+    animation: background-fade 7s ease;
   }
 
   .arrow {
-    animation: opacity-fade 10s ease;
+    animation: opacity-fade 7s ease;
   }
 
   @keyframes opacity-fade {
@@ -33,26 +33,36 @@
 
   @keyframes background-fade {
     0% {
-      background-color: rgba(0,0,0,1);
+      background-color: rgba(0, 0, 0, 1);
     }
     25% {
-      background-color: rgba(0,0,0,1);
+      background-color: rgba(0, 0, 0, 1);
     }
     100% {
-      background-color: rgba(0,0,0,0);
+      background-color: rgba(0, 0, 0, 0);
     }
   }
 </style>
 
 <script>
   import DownArrow from "../components/DownArrow.svelte";
+  import {scrollto} from "svelte-scrollto";
+
+  function toggleHud(event) {
+    if (event.key !== "d")
+      return;
+
+    document.getElementById("hud").classList.toggle("opacity-0")
+  }
 </script>
 
+<svelte:window on:keydown={toggleHud}/>
+
 <section id="header" class="min-h-screen text-white flex justify-center px-5">
-  <div class="my-auto flex">
-    <div class="arrow my-auto mr-0 invisible w-0 sm:visible sm:w-auto sm:mr-8">
+  <div id="hud" class="my-auto flex">
+    <a href="#introduction" use:scrollto={'#introduction'} class="arrow my-auto mr-0 invisible w-0 sm:visible sm:w-auto sm:mr-8">
       <DownArrow/>
-    </div>
+    </a>
     <div class="pb-1 animate__animated animate__fadeIn">
       <h1 class="text-5xl font-semibold font-fira">CosmicSail</h1>
       <p class="text-lg">
